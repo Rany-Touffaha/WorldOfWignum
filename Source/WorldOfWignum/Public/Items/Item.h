92b8f6c141a93f6dbf3addd2714a7c7f05f33f9f
Rany-Touffaha <r_touffaha@hotmail.com>
2024-03-10 19:28:29 +0000

    Allow variables in the Item class to be visible and editable in the object blueprint and in the object instance.

--- Source/WorldOfWignum/Public/Items/Item.h
+++ Source/WorldOfWignum/Public/Items/Item.h
@@ -16,8 +16,14 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
-private:
-	float RunningTime;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Sine Parameters")
 	float Amplitude = 0.25f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Sine Parameters")
 	float TimeConstant = 5.f;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float RunningTime;
+	
 };
