b2d0dbb02dd12404393db1ebed9f487b329271d6
Rany-Touffaha <r_touffaha@hotmail.com>
2024-03-10 18:34:14 +0000

    Implement a hovering mechanic for the item class using a sine function.

--- Source/WorldOfWignum/Private/Items/Item.cpp
+++ Source/WorldOfWignum/Private/Items/Item.cpp
@@ -14,5 +14,12 @@ void AItem::BeginPlay()
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	RunningTime += DeltaTime;
+	const float DeltaZ = Amplitude* FMath::Sin(RunningTime * TimeConstant);
+	AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
+
+	DRAW_SPHERE_SINGLE_FRAME(GetActorLocation())
+	DRAW_VECTOR_SINGLE_FRAME(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f)
 }
 
