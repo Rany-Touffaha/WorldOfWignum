411e46664f9c9d3ae14f5cb8b3ddafc07f344f1b
Rany-Touffaha <r_touffaha@hotmail.com>
2024-03-11 11:50:56 +0000

    Create a Bird Class and its blueprint. Update .gitignore to avoid uploading large assets.

---
+++ Source/WorldOfWignum/Private/Pawns/Bird.cpp
@@ -0,0 +1,23 @@
+#include "Pawns/Bird.h"
+
+ABird::ABird()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABird::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABird::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
