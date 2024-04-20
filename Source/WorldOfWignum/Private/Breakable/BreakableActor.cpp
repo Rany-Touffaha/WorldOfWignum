afae59002583f3b07b0e0722ac06050cdf45bd19
Rany-Touffaha <r_touffaha@hotmail.com>
2024-04-20 20:12:19 +0200

    Create a BreakableActor class.

---
+++ Source/WorldOfWignum/Private/Breakable/BreakableActor.cpp
@@ -0,0 +1,31 @@
+// World of Wignum by Rany Touffaha
+
+#include "Breakable/BreakableActor.h"
+#include "GeometryCollection/GeometryCollectionComponent.h"
+
+ABreakableActor::ABreakableActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	GeometryCollection = CreateDefaultSubobject<UGeometryCollectionComponent>(TEXT("GeomtryCollection"));
+	SetRootComponent(GeometryCollection);
+	
+	GeometryCollection->SetGenerateOverlapEvents(true);
+	GeometryCollection->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECR_Ignore);
+}
+
+void ABreakableActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABreakableActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABreakableActor::GetHit(const FVector& ImpactPoint)
+{
+	
+}
+
---
+++ Source/WorldOfWignum/Public/Breakable/BreakableActor.h
@@ -0,0 +1,30 @@
+// World of Wignum by Rany Touffaha
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interfaces/HitInterface.h"
+#include "BreakableActor.generated.h"
+
+class UGeometryCollectionComponent;
+
+UCLASS()
+class WORLDOFWIGNUM_API ABreakableActor : public AActor, public IHitInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	ABreakableActor();
+	virtual void Tick(float DeltaTime) override;
+
+	// Function that handles breakable item reaction when getting hit
+	virtual void GetHit(const FVector& ImpactPoint) override;
+	
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UGeometryCollectionComponent* GeometryCollection;
+};
